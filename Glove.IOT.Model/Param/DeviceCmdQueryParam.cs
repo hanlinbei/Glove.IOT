cb9f596bdf9f7754fbe57686033c5e4172d6bea6
hanlinbei <752572210@qq.com>
2019-07-15 20:59:12 +0800

    盛传程序文件

---
+++ Glove.IOT.Model/Param/DeviceCmdQueryParam.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glove.IOT.Model.Param
+{
+    public class DeviceCmdQueryParam:BaseParam
+    {
+    }
+}
